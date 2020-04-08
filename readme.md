59be43b5f8bc897166e1b080ceef6b0e9c834fc7
taoqf <tao_qiufeng@126.com>
2020-04-08 16:12:02 +0800

    init

--- readme.md
+++ readme.md
@@ -1,1 +1,92 @@
 # 滑动列表控件
+
+<!-- TOC -->
+
+- [1. 使用方法](#1-使用方法)
+- [2. 属性](#2-属性)
+- [3. 方法](#3-方法)
+	- [3.1. `open()`](#31-open)
+	- [3.2. `close()`](#32-close)
+- [4. 事件](#4-事件)
+	- [4.1. `mmwe-open`](#41-mmwe-open)
+	- [4.2. `mmwe-close`](#42-mmwe-close)
+
+<!-- /TOC -->
+
+小程序自定义组件
+
+> 使用此组件需要依赖小程序基础库 2.2.1 以上版本，同时依赖开发者工具的 npm 构建。具体详情可查阅[官方 npm 文档](https://developers.weixin.qq.com/miniprogram/dev/devtools/npm.html)。
+
+[使用示例](https://developers.weixin.qq.com/s/HlZlvOmO7g42)
+
+## 1. 使用方法
+
+1. 在WXML文件中添加控件
+
+    使用vscode插件的方式添加代码,定位到要添加该控件的代码处,按快捷键`alt+t w`添加控件代码片段
+
+1. 注意
+
+	- 每一个 mm-000001 提供两个`<slot>`节点，用于承载组件引用时提供的子节点。
+
+	- left 节点用于承载静止时 mm-000001 所展示的节点，此节点的宽高应与传入 mm-000001 的宽高相同。
+
+	- right 节点用于承载滑动时所展示的节点，其宽度应于传入 mm-000001 的 slideWidth 相同。
+
+	``` xml
+	<mm-000001 class="slide" width="320" height="100" slideWidth="200">
+	<view slot="left">这里是插入到组内容</view>
+	<view slot="right">
+		<view>标为已读</view>
+		<view>删除</view>
+	</view>
+	</mm-000001>
+	```
+
+## 2. 属性
+
+| 属性名 | 类型 | 默认值 | 是否必须 | 说明
+|-------|-----|-------|---------|------|
+| width | Number | 显示屏幕的宽度 | 是 | mm-000001组件的宽度 |
+| height | Number | 0 | 是 | mm-000001组件的高度 |
+| slide-width | Number | 0 | 是 | 滑动展示区域的宽度（默认高度与控件相同）|
+| wid | String | 无 | 是 | 控件标示|
+
+## 3. 方法
+
+### 3.1. `open()`
+
+展开菜单
+
+使用示例：
+
+wxml
+
+```xml
+<mm-000001 width="750" height="110" slide-width="500" class='class-name' wid='widget-001'>
+	<view slot="left" class="l">
+		内容
+	</view>
+	<view slot="right" class="r" style="background:#ccc;width:250rpx;">
+		<view>菜单</view>
+	</view>
+</mm-000001>
+```
+
+### 3.2. `close()`
+
+关闭菜单
+
+## 4. 事件
+
+### 4.1. `mmwe-open`
+
+菜单展开事件
+
+可以通过 e.detail.wid获取控件的唯一标示
+
+### 4.2. `mmwe-close`
+
+菜单折叠事件
+
+可以通过 e.detail.wid获取控件的唯一标示
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+const _windowWidth = wx.getSystemInfoSync().windowWidth;
+
+interface IData {
+	_slideWidth: number;
+	_endX: number;
+	_startX: number;
+	_threshold: number;
+	_viewWidth: number;
+}
+
 /**
  * 滑动列表控件
  */
@@ -6,10 +16,116 @@ Component({
 	 * 组件的属性列表
 	 */
 	options: {
+		multipleSlots: true
 	},
 	properties: {
+		//  组件显示区域的高度
+		height: {
+			type: Number,
+			value: 0
+		},
+		//  组件滑动显示区域的宽度
+		slideWidth: {
+			type: Number,
+			value: 0
+		},
+		//  组件显示区域的宽度
+		wid: String,
+		width: {
+			type: Number,
+			value: _windowWidth
+		}
 	},
 
+	/**
+	 * 组件的初始数据
+	 */
+	data: {
+		//  movable-view是否可以出界
+		out: false,
+		viewWidth: _windowWidth,
+		//  movable-view偏移量
+		x: 0,
+	},
+
+	/**
+	 * 组件的方法列表
+	 */
+	//    获取右侧滑动显示区域的宽度
+	ready() {
+		const query = wx.createSelectorQuery().in(this);
+		query.select('.right').boundingClientRect((res: wx.NodesRefRect) => {
+			const t = this as unknown as IData;
+			t._slideWidth = res.width;
+			t._threshold = res.width / 2;
+			const viewWidth = this.data.width + res.width * (750 / _windowWidth);
+			this.setData({
+				viewWidth
+			});
+		}).exec();
+	},
 	methods: {
+		onTouchStart(e: wx.TouchStartEvent) {
+			(this as unknown as IData)._startX = e.changedTouches[0].pageX;
+		},
+		//  当滑动范围超过阈值自动完成剩余滑动
+		onTouchEnd(e: wx.TouchEndEvent) {
+			const t = this as unknown as IData;
+			t._endX = e.changedTouches[0].pageX;
+			const { _endX, _startX, _threshold } = t;
+			if (_endX > _startX && this.data.out === false && this.data.x === 0) {
+				this.triggerEvent('mmwe-close', { wid: this.data.wid }, { bubbles: true });
+				return;
+			}
+			if (_startX - _endX >= _threshold) {
+				this.setData({
+					x: -t._slideWidth
+				});
+				this.triggerEvent('mmwe-open', { wid: this.data.wid }, { bubbles: true });
+			} else if (_startX - _endX < _threshold && _startX - _endX > 0) {
+				this.setData({
+					x: 0
+				});
+				this.triggerEvent('mmwe-close', { wid: this.data.wid }, { bubbles: true });
+			} else if (_endX - _startX >= _threshold) {
+				this.setData({
+					x: 0
+				});
+				this.triggerEvent('mmwe-close', { wid: this.data.wid }, { bubbles: true });
+			} else if (_endX - _startX < _threshold && _endX - _startX > 0) {
+				this.setData({
+					x: -t._slideWidth
+				});
+				this.triggerEvent('mmwe-open', { wid: this.data.wid }, { bubbles: true });
+			}
+		},
+		//  根据滑动的范围设定是否允许movable-view出界
+		onChange(e: wx.CustomEvent<'change', {
+			x: number;
+		}>) {
+			const t = this as unknown as IData;
+			if (!this.data.out && e.detail.x < -t._threshold) {
+				this.setData({
+					out: true
+				});
+			} else if (this.data.out && e.detail.x >= -t._threshold) {
+				this.setData({
+					out: false
+				});
+			}
+		},
+		close() {
+			this.setData({
+				out: false,
+				x: 0
+			});
+		},
+		open() {
+			const t = this as unknown as IData;
+			this.setData({
+				out: true,
+				x: -t._slideWidth
+			});
+		}
 	}
 });
